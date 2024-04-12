0d9bf8e5454482fa067c591d9e628beb9e706de6
SayanSingha07 <sayansinghbabu143@gmail.com>
2024-04-12 20:37:42 +0530

    3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color,setColor]=useState("olive")
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="w-full h-screen  duration-1000" 
+    style={{ backgroundColor : color}}
+    > 
+    <div className="fixed flex flex-wrap justify-center
+    bottom-12 inset-x-0 px-2">
+       <div className=" flex flex-wrap justify-center gap-2 shadow-lg bg-white px-3 py-2 rounded-xl ">
+        <button onClick={ ()=>setColor("red")}
+        className="outerline-none px-4 py-1 rounded-full text-white "style={{backgroundColor:"red"}}>
+          red
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+        <button onClick={ ()=>setColor("yellow")}
+        className="outerline-none px-4 py-1 rounded-full text-white "style={{backgroundColor:"yellow"}}>
+        yellow
+        </button>
+        <button onClick={ ()=>setColor("blue")}
+         className="outerline-none px-4 py-1 rounded-full text-white "style={{backgroundColor:"blue"}}>
+        blue
+        </button>
+        <button onClick={ ()=>setColor("black")}
+        className="outerline-none px-4 py-1 rounded-full text-white "style={{backgroundColor:"black"}}>
+          black
+        </button>
+        
+        
+        
+        </div> 
+        </div>
+    </div>
   )
 }
 
